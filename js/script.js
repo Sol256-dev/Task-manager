f4b1a62a01539629796badb8330d7da38a64e1ad
Sol256-dev <sol.malinga84@gmail.com>
2022-11-22 14:02:01 +0300

    empty input field reject

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ let inputField = document.querySelector(".add-task"),
   tempMsg = document.querySelector(".temp-msg");
 
 function addToList() {
-  if (ul.childNodes != li) {
-    ul.appendChild(li);
-    li.innerHTML = inputField.value;
-    tempMsg.classList.add("hide");
-  } else {
-    console.log("hello");
+  if (inputField.value === "") { //check if the input field is empty
+    alert("Please enter a task!");
+  } else { // if not empty perform tasks below
+    li.innerHTML = inputField.value; // equate the value of the input field to the list item
+    ul.appendChild(li); // add li item to the ul
+    tempMsg.classList.add("hide"); // add .hide class to tempMsg
   }
 
   inputField.value = "";
