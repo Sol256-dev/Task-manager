b5229c8ae9750fa8bd8e90a0bb54dbc441ae88e1
Sol256-dev <sol.malinga84@gmail.com>
2022-11-22 15:32:39 +0300

    changes to the list, task area height

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
-let inputField = document.querySelector(".add-task"),
-  ul = document.querySelector("ul"),
-  li = document.createElement("li"),
-  tempMsg = document.querySelector(".temp-msg");
-
-function addToList() {
-  if (inputField.value === "") { //check if the input field is empty
-    alert("Please enter a task!");
-  } else { // if not empty perform tasks below
-    li.innerHTML = inputField.value; // equate the value of the input field to the list item
-    ul.appendChild(li); // add li item to the ul
-    tempMsg.classList.add("hide"); // add .hide class to tempMsg
-  }
-
-  inputField.value = "";
-}
-
-let submitBtn = document
+let subBtn = document
   .querySelector(".btn")
-  .addEventListener("click", addToList);
+  .addEventListener("click", function () {
+    let ul = document.querySelector("ul"),
+      inputField = document.querySelector(".add-task").value,
+      txtNode = document.createTextNode(inputField),
+      li = document.createElement("li"),
+      tempMsg = document.querySelector(".temp-msg");
+
+    if (inputField === "") {
+      alert("Please enter a task!");
+    } else {
+      tempMsg.classList.add("hide");
+      li.appendChild(txtNode);
+      ul.appendChild(li);
+    }
+    inputField.value = "";
+  });
