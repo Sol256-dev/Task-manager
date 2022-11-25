e61709ebb7023469909d95b3b6aa27f52896261f
Sol256-dev <sol.malinga84@gmail.com>
2022-11-25 23:33:26 +0300

    stable working add button

--- js/script.js
+++ js/script.js
@@ -1,73 +1,54 @@
-// // =====================================================================
-// // having all list item stored with one key...sort of like an array
+// array to store input values
+let arr = [];
 
-// let listItems = [];
-// let singleElementFromStoredArray = ""; // to store the values from localstorage
+let inputField = document.querySelector(".add-task");
 
-// // function to call a click event
-// let subBtn = document.querySelector(".btn").addEventListener("click", () => {
-//   let ul = document.querySelector("ul"),
-//     inputField = document.querySelector(".add-task").value;
+// fx to store list values from input field in an array
+let captureInputValues = () => {
+  arr.push(inputField.value);
+  // call fx to store above arr in localStorage
+  storeArrInLocalstorage();
+};
 
-//   listItems.push(inputField);
-//   localStorage.setItem("value", JSON.stringify(listItems));
-//   let arrStored = JSON.parse(localStorage.getItem("value"));
+// fx to store arr in localStorage
+let storeArrInLocalstorage = () => {
+  let arrStr = JSON.stringify(arr);
+  window.localStorage.setItem("keyValue", arrStr);
+  //   call displayToList fx
+  displayToList();
+};
 
-//   arrStored.forEach((element) => { // to check for matching element with the input value
-//     if (element === inputField) {
-//       singleElementFromStoredArray = element;
-//     }
-//   });
+// fx to pick items from localStorage and display on a list
+let displayToList = () => {
+  // UI elements
+  const ul = document.querySelector("ul"),
+    li = document.createElement("li"),
+    tempMsg = document.querySelector(".temp-msg");
 
-//   let txtNode = document.createTextNode(singleElementFromStoredArray),
-//     li = document.createElement("li"),
-//     tempMsg = document.querySelector(".temp-msg"),
-//     //input field querySelector
-//     inpf = document.querySelector(".add-task");
-
-//   if (inputField === "") {
-//     alert("Please enter a task!");
-//   } else {
-//     tempMsg.classList.add("hide");
-//     li.appendChild(txtNode);
-//     ul.appendChild(li);
-//   }
-
-//   // reset the input field
-//   inpf.value = "";
-// });
-
-// =====================================================================
-// having each list item stored with its own key
-
-let list = [];
-
-// when button is clicked, this runs first
-let btn = document.querySelector(".btn").addEventListener("click", () => {
-  let inputField = document.querySelector(".add-task");// ====
-  let li = document.createElement("li"),
-    ul = document.querySelector("ul");
-
-  list.push(inputField.value);
-
-  for (let i = 0; i < list.length; i++) {
-    window.localStorage.setItem(i, JSON.stringify(list[i]));
-
-    let localStore = window.localStorage.getItem(i),
-      tempMsg = document.querySelector(".temp-msg"),
-      txtNode = document.createTextNode(JSON.parse(localStore));
-
-    console.log(txtNode);
-
+  if (tempMsg.classList.contains("hide") && inputField.value === "") {
+    alert("Please enter a task!");
+  } else {
+    // tempStore stores an array of the localStorage values
+    let tempStore = JSON.parse(localStorage.getItem("keyValue"));
     tempMsg.classList.add("hide");
-
-    li.appendChild(txtNode);
-    ul.appendChild(li);
-
-    // reset the input field after button click
-    inputField.value = "";
-    // break;
+    // use foreach loop to iterate through the tempStore array
+    tempStore.forEach((element) => {
+      if (element === inputField.value) {
+        const txtNode = document.createTextNode(element);
+        li.appendChild(txtNode);
+        ul.appendChild(li);
+        clearInputField();
+      }
+    });
   }
-});
+};
 
-    
+// clear input field
+let clearInputField = () => {
+  return (inputField.value = "");
+};
+
+// add button element defined and functions defined
+const addBtn = document.querySelector(".btn").addEventListener("click", () => {
+  captureInputValues();
+});
